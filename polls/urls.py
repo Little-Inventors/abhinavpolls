f2e94a990457a9b85cb269d7e35919607d893985
Abhinav P <abhinav.pentyala@gmail.com>
2021-08-07 11:48:58 -0400

    why css not working :(
    :(:((:(:(:(::(

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'), 
     path('<int:question_id>/results/', views.result, name='result'),
-   path('<int:question_id>/vote/', views.vote, name='vote'),]
+   path('<int:question_id>/vote/', views.vote, name='vote'),] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
